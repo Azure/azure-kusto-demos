59619c65e55af1672584ce3cdf4af02be67289bf
Ariel Yehezkely <37837385+ArielYehezkely@users.noreply.github.com>
2018-12-24 18:01:27 +0200

    Create run.sh

---
+++ eventhub-python/github-events-demo/DockerFile/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home/
+
+git clone https://github.com/Azure/azure-kusto-demos.git 
+
+cp /home/config/EventHubsConfig.json /home/azure-kusto-demos/eventhub-python/github-events-demo/config/
+
+cd /home/azure-kusto-demos/eventhub-python/github-events-demo/
+
+python crawl_send_eventhub.py
